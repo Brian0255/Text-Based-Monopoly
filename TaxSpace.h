459b16e0eed3e38fb0d06789978eae50c0541a26
brian-neveu2 <61294586+brian-neveu2@users.noreply.github.com>
2020-03-08 12:46:14 -0400

    Update TaxSpace.h

--- TaxSpace.h
+++ TaxSpace.h
@@ -6,18 +6,10 @@
 class TaxSpace : public Space
 {
 public:
-	//the constructor
 	TaxSpace(std::string name, std::string type, int cost);
-
-	//overrided destructor
 	~TaxSpace();
 private:
-	//the name of the space
 	std::string name;
-
-	//the type of the space
 	std::string type;
-
-	//the cost for the taxes
 	int cost;
 };
