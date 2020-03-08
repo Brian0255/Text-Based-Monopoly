aa069ae03e12d6a8a55be59f0f38c8682f87e11d
brian-neveu2 <61294586+brian-neveu2@users.noreply.github.com>
2020-03-08 12:45:43 -0400

    Update SpaceHandler.h

--- SpaceHandler.h
+++ SpaceHandler.h
@@ -8,6 +8,5 @@ class SpaceHandler
 {
 public:
 	static void handle(Game& game, Player& player, Space* space, std::vector<Player>& curPlayers);
-	//handles player landing on spaces. more explanation in the cpp
 private:
 };
