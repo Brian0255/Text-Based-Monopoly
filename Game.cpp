a61712e12c8f11ce864987a9da40426e1230b158
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-01-15 16:24:22 -0500

    finish refactor of battle methods

--- Game.cpp
+++ Game.cpp
@@ -13,6 +13,49 @@
 
 using namespace std;
 
+vector<string> Game::battleDialogChoices = 
+{
+	" throws their money at the other player! They deal ",
+	" throws the board at the other player! They deal ",
+	" hits the other player with a baseball bat! They deal ",
+	" body slams the other player! It's a smackdown! They deal ",
+	" throws a bomb at the other player! They deal ",
+	" smashes the board over the other player's head! OOF! They deal ",
+	" throws a miniature house at the other player! Ouch! They deal ",
+	" trips over a banana peel right into the other player! They deal ",
+	" takes the banker's briefcase and hits the other player with it! They deal ",
+	" tells the other player how worthless they are! They deal ",
+	" takes their shoe off and throws it at the other player! They deal "
+};
+
+vector<string> Game::playerMobChoices = 
+{
+	" throws their money at the mob of people! They deal ",
+	" throws the board at the mob of people! They deal ",
+	" tells a horribly boring story! They deal ",
+	" body slams the mob of people! It's a smackdown! They deal ",
+	" throws a water balloon at the mob of people! They deal ",
+	" smashes the board over the mob of people! OOF! They deal ",
+	" throws several miniature houses at the mob of people! Ouch! They deal ",
+	" trips over a banana peel right into the mob of people! They deal ",
+	" takes the banker's briefcase and hits the mob of people with it! They deal ",
+	" tells the mob of people how worthless they are! They deal ",
+	" takes their shoe off and throws it at the mob of people! They deal "
+};
+vector<string> Game::mobChoices = 
+{
+	" damages the attacker with pitch forks! They deal ",
+	" drops a bucket of water on the attacker! They deal ",
+	" shoots flaming arrows at the attacker! They deal ",
+	" sends a pack of angry dogs at the attacker! They deal ",
+	" throws rocks at the attacker! They deal ",
+	" shouts angrily at the attacker! They deal ",
+	" throws spears at the attacker! They deal ",
+	" catches the attacker in a trap! They deal ",
+	" throws their two cents at the attacker! They deal ",
+	" jumps at the same time and causes an earthquake. OOF! They deal "
+};
+
 Game::Game() {
 	initializeSpaces();
 	initializeBoard();
@@ -982,7 +1025,6 @@ void Game::doStuffBeforeRoll(Player& player) {
 }
 
 vector<Player> Game::setup() {
-	vector<Player> players;
 	system("CLS");
 	int numPlayers = Utilities::getIntInRange("How many players will be playing?(0-6):", 0, 6);
 	int possibleAI = 6 - numPlayers;
@@ -998,14 +1040,14 @@ vector<Player> Game::setup() {
 	bool infCash = setupCheats();
 	int toLoan = (infCash == true) ? 1'000'000 : 1500;
 	cout << "The banker will now loan every player $" + to_string(toLoan) + " to start the game." << endl;
-	for (auto& player : players) {
+	for (auto& player : curPlayers) {
 		player.setCash(toLoan);
 	}
 	Sleep(3000);
 	cout << "The board will now be generated in a few seconds." << endl;
 	Sleep(3000);
 	system("CLS");
-	return players;
+	return curPlayers;
 }
 
 int Game::setupAI(int possibleAI) {
@@ -1412,25 +1454,68 @@ vector<Space*> Game::getSpaces() {
 }
 
 void Game::battle(Player& player1, Player& player2) {
-	vector<string> battleDialogChoices
-	{
-		" throws their money at the other player! They deal ",
-		" throws the board at the other player! They deal ",
-		" hits the other player with a baseball bat! They deal ",
-		" body slams the other player! It's a smackdown! They deal ",
-		" throws a bomb at the other player! They deal ",
-		" smashes the board over the other player's head! OOF! They deal ",
-		" throws a miniature house at the other player! Ouch! They deal ",
-		" trips over a banana peel right into the other player! They deal ",
-		" takes the banker's briefcase and hits the other player with it! They deal ",
-		" tells the other player how worthless they are! They deal ",
-		" takes their shoe off and throws it at the other player! They deal "
-	};
-	player1.setHP(100);
-	player2.setHP(100);
 
-	player1.setHP(player1.getHP() + calculateAssets(player1) - (player1.getCash() / 10));
-	player2.setHP(player1.getHP() + calculateAssets(player2) - (player2.getCash() / 10));
+	setupHPDefense(player1, player2);
+	preBattleDialog(player1, player2);
+
+	bool doneBattling{ false };
+	bool player1Turn{ true };
+	while (!doneBattling) {
+		displayBoard();
+		if (!player1Turn) {
+			doneBattling = runPlayerBattleTurn(player2, player1, player1Turn);
+		}
+		doneBattling = runPlayerBattleTurn(player1, player2, player1Turn);
+		player1Turn = !player1Turn;
+	}
+}
+
+bool Game::runPlayerBattleTurn(Player& player2, Player& player1, bool player1Turn) {
+	int damage = (rand() % 11) + 30;
+	int toSubtract = player2.getDefense() / 5;
+	damage -= toSubtract;
+	player2.setHP(player2.getHP() - damage);
+
+	//this is done to make sure the order of HP displayed is always the same despite whose turn it is(easier to keep track of)
+	Player first = (player1Turn) ? player1 : player2;
+	Player second = (player1Turn) ? player2 : player1;
+	cout << first.getName() << "'s HP: " << first.getHP() << endl;
+	cout << second.getName() << "'s HP: " << second.getHP() << endl << endl;
+	cout << player1.getName() + Utilities::getRandomElement(battleDialogChoices) + to_string(damage) + " damage to " + player2.getName() + "!!!";
+	Sleep(5000);
+	if (player2.getHP() <= 0) {
+		rewardBattleWinner(player2, player1);
+		return true;
+	}
+	return false;
+}
+
+void Game::rewardBattleWinner(Player& player2, Player& player1) {
+	int toReceive = player2.getCash() / 5;
+	player2.deductCash(toReceive);
+	player1.addCash(toReceive);
+	setInfo(player1);
+	displayBoard();
+	cout << player1.getName() << " has won! They get 20% of " << player2.getName() << "'s money and receive " << to_string(toReceive) << " dollars!" << endl;
+	Sleep(4000);
+}
+
+void Game::preBattleDialog(Player& player1, Player& player2, bool mobAttack) {
+	displayBoard();
+	cout << player1.getName() << ", you will face off against..." << endl;
+	Sleep(3000);
+	string secondName = (mobAttack) ? "the mob" : player2.getName();
+	cout << secondName << "!!!" << endl;
+	Sleep(2000);
+	cout << "3..."; Sleep(1000);
+	cout << "2..."; Sleep(1000);
+	cout << "1..."; Sleep(1000);
+	cout << "FIGHT!!!"; Sleep(2000);
+}
+
+void Game::setupHPDefense(Player& player1, Player& player2) {
+	player1.setHP(100 + calculateAssets(player1) - (player1.getCash() / 10));
+	player2.setHP(100 + calculateAssets(player2) - (player2.getCash() / 10));
 
 	vector<Space*> properties = Utilities::getProperty(player1, spaces);
 	for (auto& prop : properties) {
@@ -1440,99 +1525,100 @@ void Game::battle(Player& player1, Player& player2) {
 	for (auto& prop : properties) {
 		player2.setDefense(player2.getDefense() + prop->getNumTurrets() * 10);
 	}
-
-	displayBoard();
-	cout << player1.getName() << ", you will face off against..." << endl;
-	Sleep(3000);
-	cout << player2.getName() << "!!!" << endl;
-	Sleep(2000);
-	cout << "3..."; Sleep(1000);
-	cout << "2..."; Sleep(1000);
-	cout << "1..."; Sleep(1000);
-	cout << "FIGHT!!!"; Sleep(2000);
-	bool doneBattling{ false };
-	bool player1Turn{ true };
-	while (!doneBattling) {
-		displayBoard();
-		int damage = (rand() % 11) + 30;
-		if (player1Turn) {
-			int toSubtract = player2.getDefense() / 5;
-			damage -= toSubtract;
-			displayBoard();
-			player2.setHP(player2.getHP() - damage);
-			cout << player1.getName() << "'s HP: " << player1.getHP() << endl;
-			cout << player2.getName() << "'s HP: " << player2.getHP() << endl << endl;
-			cout << player1.getName() + Utilities::getRandomElement(battleDialogChoices) + to_string(damage) + " damage to " + player2.getName() + "!!!";
-			Sleep(5000);
-			if (player2.getHP() <= 0) {
-				doneBattling = true;
-				int toReceive = player2.getCash() / 5;
-				player2.deductCash(toReceive);
-				player1.addCash(toReceive);
-				setInfo(player1);
-				displayBoard();
-				cout << player1.getName() << " has won! They get 20% of " << player2.getName() << "'s money and receive " << to_string(toReceive) << " dollars!" << endl;
-				Sleep(4000);
-			}
-			else {
-				player1Turn = false;
-			}
-		}
-		else {
-			int toSubtract = player1.getDefense() / 5;
-			damage -= toSubtract;
-			player1.setHP(player1.getHP() - damage);
-			displayBoard();
-			cout << player1.getName() << "'s HP: " << player1.getHP() << endl;
-			cout << player2.getName() << "'s HP: " << player2.getHP() << endl << endl;
-			cout << player2.getName() + Utilities::getRandomElement(battleDialogChoices) + to_string(damage) + " damage to " + player1.getName() + "!!!";
-			Sleep(5000);
-
-			if (player1.getHP() <= 0) {
-				doneBattling = true;
-				int toReceive = player1.getCash() / 5;
-				player1.deductCash(toReceive);
-				player2.addCash(toReceive);
-				setInfo(player1);
-				displayBoard();
-				cout << player2.getName() << " has won! They get 20% of " << player1.getName() << "'s money and receive " << to_string(toReceive) << " dollars!" << endl;
-				Sleep(4000);
-			}
-			else {
-				player1Turn = true;
-			}
-		}
-	}
 }
 
 void Game::attackProp(Player& player, Player& mob) {
-	vector<string> playerChoices
-	{
-		" throws their money at the mob of people! They deal ",
-		" throws the board at the mob of people! They deal ",
-		" hits each person in the mob of people with a baseball bat! They deal ",
-		" body slams the mob of people! It's a smackdown! They deal ",
-		" throws a bomb at the mob of people! They deal ",
-		" smashes the board over the mob of people! OOF! They deal ",
-		" throws several miniature houses at the mob of people! Ouch! They deal ",
-		" trips over a banana peel right into the mob of people! They deal ",
-		" takes the banker's briefcase and hits the mob of people with it! They deal ",
-		" tells the mob of people how worthless they are! They deal ",
-		" takes their shoe off and throws it at the mob of people! They deal "
-	};
-	vector<string> mobChoices
-	{
-		" stabs the attacker with pitch forks! They deal ",
-		" lights the attacker on fire! They deal ",
-		" shoots flaming arrows at the attacker! They deal ",
-		" sends a pack of angry dogs at the attacker! They deal ",
-		" throws rocks at the attacker! They deal ",
-		" tries to drown the attacker! They deal ",
-		" throws spears at the attacker! They deal ",
-		" catches the attacker in a trap! They deal ",
-		" throws their two cents at the attacker! They deal ",
-		" jumps at the same time and causes an earthquake. OOF! They deal "
-	};
+	setupHPDefensePropAttack(player, mob);
+	preBattleDialog(player, mob, true);
+
+	bool doneBattling{ false };
+	bool playerTurn{ true };
+	Space* prop = spaces[player.getCurrentSpot()];
+	while (!doneBattling) {
+		displayBoard();
+		doneBattling = runMobBattleTurn(playerTurn, mob, player, prop);
+		playerTurn = !playerTurn;
+	}
+}
+
+bool Game::runMobBattleTurn(bool& playerTurn, Player& mob, Player& player, Space* prop) {
+	bool doneBattling{ false };
+	if (playerTurn) {
+		damageMob(mob, player);
+		if (mob.getHP() <= 0) {
+			awardPlayerMobProperty(player, prop);
+			doneBattling = true;
+		}
+	}
+	else {
+		attackPlayerAsMob(player, mob, prop);
+		if (player.getHP() <= 0) {
+			awardMobDefending(player);
+			doneBattling = true;
+		}
+	}
+	return doneBattling;
+}
+
+void Game::awardMobDefending(Player& player) {
+	player.deductCash(player.getCash() / 3);
+	setInfo(player);
+	displayBoard();
+	cout << "The mob has defended the attack! Huzzah! The mob robs the attacker of 30% of their money." << endl;
+	Sleep(4000);
+}
+
+void Game::attackPlayerAsMob(Player& player, Player& mob, Space* prop) {
+	int damage = (rand() % 20) + 30;
+	int turretDamage = damage;
+	int toSubtract = player.getDefense() / 5;
+	damage -= toSubtract;
+	player.setHP(player.getHP() - damage);
+	displayBoard();
+	cout << player.getName() << "'s HP: " << player.getHP() << endl;
+	cout << "The mob's HP: " << mob.getHP() << endl << endl;
+	cout << "The mob" + Utilities::getRandomElement(mobChoices) + to_string(damage) + " damage to " + player.getName() + "!!!";
+	if (prop->getNumTurrets() > 0) {
+		player.setHP(player.getHP() - turretDamage);
+		cout << "The turret on the property also fires for " << turretDamage << " damage to the attacker." << endl;
+	}
+	Sleep(5000);
+}
+
+void Game::awardPlayerMobProperty(Player& player, Space* prop) {
+	displayBoard();
+	cout << "The player successfully attacked the property!" << endl;
+	cout << "Since you were so daring, you must pay twice the cost of the property, or go bankrupt." << endl;
+	Sleep(3000);
+	player.deductCash(prop->getCost() * 2);
+	setInfo(player);
+	displayBoard();
+	if (player.getCash() <= 0) {
+		cout << "You went bankrupt! Maybe you should have left them alone..." << endl;
+		player.setBankruptcy(true);
+		Utilities::resetOwnership(player, spaces);
+		Sleep(3000);
+	}
+	else {
+		prop->setOwner(player.getName());
+		cout << "The property now belongs to you, " << player.getName() << ".\n";
+		Sleep(3000);
+	}
+}
+
+void Game::damageMob(Player& mob, Player& player) {
+	int damage = (rand() % 20) + 30;
+	int toSubtract = mob.getDefense() / 5;
+	damage -= toSubtract;
+	displayBoard();
+	mob.setHP(mob.getHP() - damage);
+	cout << player.getName() << "'s HP: " << player.getHP() << endl;
+	cout << "The mob's HP: " << mob.getHP() << endl << endl;
+	cout << player.getName() + Utilities::getRandomElement(playerMobChoices) + to_string(damage) + " damage to the mob!!!";
+	Sleep(5000);
+}
+
+void Game::setupHPDefensePropAttack(Player& player, Player& mob) {
 	player.setHP(100);
 	mob.setHP(100);
 
@@ -1548,83 +1634,5 @@ void Game::attackProp(Player& player, Player& mob) {
 	for (auto& prop : properties) {
 		mob.setDefense(mob.getDefense() + prop->getNumTurrets() * 10);
 		cout << mob.getName() << "'s defense:" << mob.getDefense() << endl;
-	}
-
-	displayBoard();
-	cout << player.getName() << ", you will now face off against the mob!!!" << endl;
-	Sleep(1000);
-	cout << "3..."; Sleep(1000);
-	cout << "2..."; Sleep(1000);
-	cout << "1..."; Sleep(1000);
-	cout << "FIGHT!!!"; Sleep(2000);
-	bool doneBattling{ false };
-	bool playerTurn{ true };
-	Space* prop = spaces[player.getCurrentSpot()];
-	while (!doneBattling) {
-		displayBoard();
-		int damage = (rand() % 20) + 30;
-		int turretDamage = damage;
-
-		if (playerTurn) {
-			int toSubtract = mob.getDefense() / 5;
-			damage -= toSubtract;
-			displayBoard();
-			mob.setHP(mob.getHP() - damage);
-			cout << player.getName() << "'s HP: " << player.getHP() << endl;
-			cout << "The mob's HP: " << mob.getHP() << endl << endl;
-			cout << player.getName() + Utilities::getRandomElement(playerChoices) + to_string(damage) + " damage to the mob!!!";
-			Sleep(5000);
-
-			if (mob.getHP() <= 0) {
-				doneBattling = true;
-				displayBoard();
-				cout << "The player successfully attacked the property!" << endl;
-				cout << "Since you were so daring, you must pay twice the cost of the property, or go bankrupt." << endl;
-				Sleep(3000);
-				player.deductCash(prop->getCost() * 2);
-				setInfo(player);
-				displayBoard();
-				if (player.getCash() <= 0) {
-					cout << "You went bankrupt! Maybe you should have left them alone..." << endl;
-					player.setBankruptcy(true);
-					Utilities::resetOwnership(player, spaces);
-					Sleep(3000);
-				}
-				else {
-					prop->setOwner(player.getName());
-					cout << "The property now belongs to you, " << player.getName() << ".\n";
-					Sleep(3000);
-				}
-			}
-			else {
-				playerTurn = false;
-			}
-		}
-		else {
-			int toSubtract = player.getDefense() / 5;
-			damage -= toSubtract;
-			player.setHP(player.getHP() - damage);
-			displayBoard();
-			cout << player.getName() << "'s HP: " << player.getHP() << endl;
-			cout << "The mob's HP: " << mob.getHP() << endl << endl;
-			cout << "The mob" + Utilities::getRandomElement(mobChoices) + to_string(damage) + " damage to " + player.getName() + "!!!";
-			if (prop->getNumTurrets() > 0) {
-				player.setHP(player.getHP() - turretDamage);
-				cout << "The turret on the property also fires for " << turretDamage << " damage to the attacker." << endl;
-			}
-			Sleep(5000);
-
-			if (player.getHP() <= 0) {
-				doneBattling = true;
-				player.deductCash(player.getCash() / 3);
-				setInfo(player);
-				displayBoard();
-				cout << "The mob has defended the attack! Huzzah! The mob robs the attacker of 30% of their money." << endl;
-				Sleep(4000);
-			}
-			else {
-				playerTurn = true;
-			}
-		}
 	}
 }
--- Game.h
+++ Game.h
@@ -9,6 +9,10 @@ class Game {
 public:
 	Game();
 
+	static std::vector<std::string> playerMobChoices;
+	static std::vector<std::string> mobChoices;
+	static std::vector<std::string> battleDialogChoices;
+
 	void initializeBoard();
 
 	void initializeSpaces();
@@ -41,7 +45,23 @@ public:
 	int calculateAssets(Player& player);
 	std::vector<Space*> getSpaces();
 	void battle(Player& player1, Player& player2);
+	bool runPlayerBattleTurn(Player& player2, Player& player1, bool player1Turn);
+	void rewardBattleWinner(Player& player2, Player& player1);
+	void preBattleDialog(Player& player1, Player& player2, bool mobAttack = false);
+	void setupHPDefense(Player& player1, Player& player2);
 	void attackProp(Player& player1, Player& propOwner);
+
+	bool runMobBattleTurn(bool& playerTurn, Player& mob, Player& player, Space* prop);
+
+	void awardMobDefending(Player& player);
+
+	void attackPlayerAsMob(Player& player, Player& mob, Space* prop);
+
+	void awardPlayerMobProperty(Player& player, Space* prop);
+
+	void damageMob(Player& mob, Player& player);
+
+	void setupHPDefensePropAttack(Player& player, Player& mob);
 
 private:
 	std::string board;
