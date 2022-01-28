43898910c64421f4b23fbbbeb49b8d753a34c4fb
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-01-27 23:49:30 -0500

    change spacing in header file for readability

--- Game.h
+++ Game.h
@@ -19,6 +19,7 @@ public:
 
 	void setInfo(Player player);
 	void doStuffBeforeRoll(Player& player);
+
 	std::vector<Player> setup();
 	int setupAI(int possibleAI);
 	bool setupCheats();
@@ -34,37 +35,41 @@ public:
 	void setGameOutput(std::vector<std::string> newOutput);
 	int getNumPlayers();
 	int rollDie();
+
 	Player play();
+
 	void runPlayerTurn(Player& player);
 	void letPlayerRoll(Player& player);
+
 	void movePlayer(Player& player);
+
 	bool runDoublesCase(Player& player, int jail);
+
 	void runPlayerTurnJail(Player& player);
 	void runPlayerJailDecision(Player& player, std::vector<std::string>& jailChoices);
 	void pay50InJail(Player& player);
-	void attemptToRollDoublesPlayer(Player& player);
 	void runAIJailDecision(Player& player);
+
+	void attemptToRollDoublesPlayer(Player& player);
 	void attemptToRollDoublesAI(Player& player);
+
 	void punishPlayerForFailingJail(Player& player);
+
 	int calculateAssets(Player& player);
 	std::vector<Space*> getSpaces();
+
 	void battle(Player& player1, Player& player2);
 	bool runPlayerBattleTurn(Player& player2, Player& player1, bool player1Turn);
 	void rewardBattleWinner(Player& player2, Player& player1);
 	void preBattleDialog(Player& player1, Player& player2, bool mobAttack = false);
 	void setupHPDefense(Player& player1, Player& player2);
+
 	void attackProp(Player& player1, Player& propOwner);
-
 	bool runMobBattleTurn(bool& playerTurn, Player& mob, Player& player, Space* prop);
-
 	void awardMobDefending(Player& player);
-
 	void attackPlayerAsMob(Player& player, Player& mob, Space* prop);
-
 	void awardPlayerMobProperty(Player& player, Space* prop);
-
 	void damageMob(Player& mob, Player& player);
-
 	void setupHPDefensePropAttack(Player& player, Player& mob);
 
 private:
